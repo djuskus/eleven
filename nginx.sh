cd7d2e86ffa54db36314c3ffe4a73247cfa24aa9
djuskus <dylan.juskus@yahoo.com>
2022-08-25 01:17:29 -0600

    revert app conf

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/sites-available/app
+sudo cp -rf app.conf /etc/nginx/sites-available/eleven
 chmod 710 /var/lib/jenkins/workspace/eleven_ci_cd
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/eleven /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
